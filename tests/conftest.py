70b5868c004f9e64c1550fd26fd53de42ed20859
Vincent Chov <vincentchov@gmail.com>
2022-12-21 18:15:49 -0500

    fix(tests): add asyncio fixture to cleanly shutdown the test app

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,35 @@
 """Test cases for the Quart app."""
+import asyncio
+
 import pytest
+import pytest_asyncio
 import quart.typing
 from quart import Quart
-from quart.typing import TestAppProtocol
 from typeguard.importhook import install_import_hook
 
 
-@pytest.fixture(name="app", scope="session")
-def _test_app_fixture() -> TestAppProtocol:
+@pytest.fixture(scope="session")
+def event_loop():
+    """Re-define's Pytest's event loop to session scope."""
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(name="app", scope="session")
+async def test_app_fixture():
+    """Quart app fixture.
+
+    Creates a test app, waits for it to finish starting, then
+    cleanly shuts it down at the end of the session.
+    """
     with install_import_hook(["quart_docker_pytest"]):
         from quart_docker_pytest.api import create_app
 
     app: Quart = create_app()
     test_app: quart.typing.TestAppProtocol = app.test_app()
-    return test_app
+    await test_app.startup()
+    await asyncio.sleep(10)
+    yield test_app
+    await test_app.shutdown()
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,4 @@
 """Test cases for the Users blueprint."""
-import asyncio
-
 import pytest
 from quart.typing import TestAppProtocol
 from quart.typing import TestClientProtocol
@@ -30,26 +28,12 @@ async def test_user_index_echoes_json(app: TestAppProtocol) -> None:
 
 
 @pytest.mark.asyncio
-async def test_user_data_empty_on_start(app: TestAppProtocol) -> None:
-    """Tests that user data is empty when the Quart app starts."""
-    client: TestClientProtocol = app.test_client()
-    response = await client.get("/users/data/")
-    assert response.status_code == 200
-    users = await response.get_json()
-    assert type(users) == list
-    assert len(users) == 0
-
-
-@pytest.mark.asyncio
 async def test_user_data_loads_after_start(app: TestAppProtocol) -> None:
     """Tests that user data is loaded by the Quart app after 10s."""
     client: TestClientProtocol = app.test_client()
-    await app.startup()
-    await asyncio.sleep(10)
     response = await client.get("/users/data/")
     users = await response.get_json()
     assert len(users) > 0
     example_record = User.from_dict(users[0])
     assert example_record.id
     assert example_record.name != ""
-    await app.shutdown()
